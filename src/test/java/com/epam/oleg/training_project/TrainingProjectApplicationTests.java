aee29c0ff9e99170679580eb6c1f4bb8225f1127
olegkornii <olegnatikeros@gmail.com>
2019-12-07 14:12:19 +0200

    Initial commit

---
+++ src/test/java/com/epam/oleg/training_project/TrainingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.oleg.training_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

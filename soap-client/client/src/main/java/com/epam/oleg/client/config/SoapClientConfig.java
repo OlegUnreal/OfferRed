494db59a9fdffdc84a09e823b339e6a59cce67f6
Kornii, Oleg [External] <Oleg.Kornii@externals.adidas.com>
2020-04-16 20:44:44 +0300

    Fixed defect with visibility during client call

--- soap-client/client/src/main/java/com/epam/oleg/client/config/SoapClientConfig.java
+++ soap-client/client/src/main/java/com/epam/oleg/client/config/SoapClientConfig.java
@@ -1,9 +1,12 @@
 package com.epam.oleg.client.config;
 
 import com.epam.oleg.client.client.UserClient;
+import com.epam.oleg.client.dto.UserSearchRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class SoapClientConfig {
@@ -11,17 +14,17 @@ public class SoapClientConfig {
     @Bean
     public Jaxb2Marshaller marshaller() {
         Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
-        jaxb2Marshaller.setContextPath("com.oleg.epam.soapclient");
+        jaxb2Marshaller.setPackagesToScan("com.oleg.epam", "io.spring.guides.gs_producing_web_service");
 
         return jaxb2Marshaller;
     }
 
     @Bean
-    public UserClient articleClient(Jaxb2Marshaller jaxb2Marshaller) {
-        UserClient articleClient = new UserClient();
-        articleClient.setDefaultUri("http://localhost:8080/ws");
-        articleClient.setMarshaller(jaxb2Marshaller);
-        articleClient.setUnmarshaller(jaxb2Marshaller);
-        return articleClient;
+    public UserClient userClient(Jaxb2Marshaller jaxb2Marshaller) {
+        UserClient userClient = new UserClient();
+        userClient.setDefaultUri("http://localhost:8080/ws");
+        userClient.setMarshaller(jaxb2Marshaller);
+        userClient.setUnmarshaller(jaxb2Marshaller);
+        return userClient;
     }
 }
